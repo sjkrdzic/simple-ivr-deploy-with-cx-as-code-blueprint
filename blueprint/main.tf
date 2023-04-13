dfa285aed71d9e7158c5a34c84d4dd489f4c3674
sjkrdzic <sjkrdzic@gmail.com>
2023-04-13 13:29:18 -0400

    changed the phone number

--- blueprint/main.tf
+++ blueprint/main.tf
@@ -21,7 +21,7 @@ resource "genesyscloud_user" "sf_johnsmith" {
   addresses {
 
     phone_numbers {
-      number     = "9205551212"
+      number     = "+19205551212"
       media_type = "PHONE"
       type       = "MOBILE"
     }
@@ -45,7 +45,7 @@ resource "genesyscloud_user" "sf_janesmith" {
   addresses {
 
     phone_numbers {
-      number     = "9205551212"
+      number     = "+19205551212"
       media_type = "PHONE"
       type       = "MOBILE"
     }
